d9b766c4b03368761457f774a10f6b957cd532b5
Victor <victorbtdias159@hotmail.com>
2024-10-16 08:57:39 -0300

    Teste branch

--- src/components/Siderbar/WriteButton.jsx
+++ src/components/Siderbar/WriteButton.jsx
@@ -2,7 +2,7 @@ const WriteButton = ({ children }) => (
   <button
     className="
       bg-primary-300 hover:bg-primary-400 
-      dark:bg-primary-500 dark:hover:bg-primary-600 
+      dark:bg-red-600 dark:hover:bg-red-950
       py-2 w-full rounded-lg transition-colors"
   >
     {children}
